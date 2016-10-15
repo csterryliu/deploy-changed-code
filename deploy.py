df1b798355c1c3553c99a14a2d9f58e363324cd6
Terry_Liu <tkuterry.liu@gmail.com>
2016-10-15 21:02:01 +0800

    backup

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from subprocess import check_output, call
+from re import split as regx_split
 import argparse
 
 def main():
@@ -28,6 +29,9 @@ def process_args():
                             default='22',
                             metavar='Port Number',
                             help='The Port Number. Default is 22.')
+    arg_parser.add_argument('--force',
+                            action='store_true',
+                            help='Force Deploy. Bypass The File Permission.')
 
     return arg_parser.parse_args()
 
@@ -36,17 +40,57 @@ def deploy_code(staged_file, args):
     tag, filename = staged_file.split('  ')
     if tag is 'R':
         old_filename, filename = filename.split(' -> ')
+        if args.force:
+            ls_output = check_output(['ssh',
+                          args.host_address,
+                          '-p', args.port,
+                          'ls -al ' + args.git_root_path + filename])
+            permission, _, _ = ls_parser(ls_output)
+            original_permission = permission_parser(permission)
+            #call(['ssh',
+            #      args.host_address,
+            #      '-p', args.port,
+            #      'sudo chmod 777 ' + args.git_root_path + old_filename])
         print 'Replace ' + old_filename + ' with ' + filename
         call(['ssh',
               args.host_address,
               '-p', args.port,
               'mv ' + args.git_root_path + old_filename + ' ' + args.git_root_path + filename])
+        #if args.force:
+            #call(['ssh',
+            #      args.host_address,
+            #      '-p', args.port,
+            #      'sudo chmod ' + original_permission  + ' ' + args.git_root_path + old_filename])
         return None
-    if args.method is 'scp':
+    if args.method == 'scp':
         print 'scp ' + filename + ' to ' + args.git_root_path
         call(['scp', '-P', args.port , filename, args.host_address + ':' + args.git_root_path + filename])
+    elif args.method == 'rsync':
+        print 'rsync ' + filename + ' to ' + args.git_root_path
+        call(['rsync', '-ave', 'ssh -p ' + args.port , filename, args.host_address + ':' + args.git_root_path + filename])
+    else:
+        print 'Unsupported method'
     return None
 
+def ls_parser(ls_output):
+    permission, _, owner, group, _, _, _, _, _, _ = regx_split('\s+', ls_output)
+    return permission, owner, group
+
+def permission_parser(permission_str):
+    permission = 0
+    permission_map = {
+        'r': 4,
+        'w': 2,
+        'x': 1,
+        '-': 0
+    }
+    for i in range(len(permission_str)):
+        if i == 0:
+            continue
+        elif i == 4 or i == 7:
+            permission *= 10
+        permission += permission_map[permission_str[i]]
+    return permission
 
 
 if __name__ == '__main__':
